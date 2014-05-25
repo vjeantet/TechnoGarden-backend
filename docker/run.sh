ab78d3c54fd8ed50693562b0c7ac8e620c0c9df5
roux <roux@concertino.(none)>
2014-05-25 12:42:58 +0200

    Ajout du flux de données dans le résultat de l'API de recherche, ajout du Dockerfile

---
+++ docker/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d -p 80:80 -p 9000:9000 technogarden
--- src/App/Services/SearchService.php
+++ src/App/Services/SearchService.php
@@ -6,7 +6,7 @@ class SearchService extends BaseService
 {
 	public function findTechnosTeamsPeopleByTerm($term)
 	{
-
+		// Compute teams
 		$sql = 'SELECT t.* 
 				FROM teams t 
 				LEFT JOIN team_techno tt ON tt.`id_team` = t.`id`
@@ -19,21 +19,25 @@ class SearchService extends BaseService
 		
 		$team_list_tmp = $this->db->fetchAll($sql,array('%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%')) ;
 		
+		// Compute teams technos
 		$sql = 'SELECT t.*
 				FROM techno t
 				INNER JOIN team_techno tt ON tt.`id_team` = ? AND tt.`id_techno` = t.`id` ;
     			';
 		
 		$team_list = array() ;
+		$team_ids = array();
+		
 		foreach($team_list_tmp as $k => $team)
 		{
 			unset($k) ;
 			$team_techno_list = $this->db->fetchAll($sql,array($team['id'])) ;
 			$team['technos'] = $team_techno_list ;
 			$team_list[] = $team ;
+			$team_ids[] = $team['id'];
 		}
 		
-
+		// Compute technos
 		$sql = 'SELECT t.* FROM techno t 
 				WHERE 
 				t.`description` like ?
@@ -43,23 +47,30 @@ class SearchService extends BaseService
 				LIMIT 5		' ;
 		$techno_list = $this->db->fetchAll($sql,array('%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%')) ;
 		
-		$sql = 'SELECT distinct u.* 
-				FROM users u 
+		// Compute events
+		$eventsService = new EventsService($this->db);
+		$events_list = $eventsService->getProximityEvents($team_ids);
+
+		// Compute persons
+		$sql = 'SELECT distinct u.*
+				FROM users u
 				LEFT JOIN team_techno tt ON tt.`id_team` = u.`id_team`
-				LEFT JOIN techno tek ON tek.`id` = tt.`id_techno` 
-				
+				LEFT JOIN techno tek ON tek.`id` = tt.`id_techno`
+		
 				WHERE
-								
+		
 				( tek.`code` like ? OR tek.`label` like ? OR tek.`type` like ? OR tek.`description` like ?)
-				
+		
 				LIMIT 5';
 		
 		$person_list = $this->db->fetchAll($sql,array('%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%')) ;
 		
+		
 		return array(
 			'teams' 	=> $team_list,
 			'technos' 	=> $techno_list,
-			'people' 	=> $person_list,
+			'events' 	=> $events_list,
+			'people'	=> $person_list
 		) ;
 	}
 }
