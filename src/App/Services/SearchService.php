872d5228ddb77a695ebe610f4a960ea21c72967d
Valere JEANTET <valere.jeantet@gmail.com>
2014-05-25 02:16:21 +0200

    amelioration de la recherche globale pour recherche les technos qu'utilisent les personnes

--- src/App/Services/SearchService.php
+++ src/App/Services/SearchService.php
@@ -7,8 +7,17 @@ class SearchService extends BaseService
 	public function findTechnosTeamsPeopleByTerm($term)
 	{
 
-		$sql = 'SELECT t.* FROM teams t WHERE t.`name` like ? LIMIT 5' ;
-		$team_list_tmp = $this->db->fetchAll($sql,array('%'.$term.'%')) ;
+		$sql = 'SELECT t.* 
+				FROM teams t 
+				LEFT JOIN team_techno tt ON tt.`id_team` = t.`id`
+				LEFT JOIN techno tek ON tek.`id` = tt.`id_techno` 
+				WHERE t.`name` like ?
+				OR
+									( tek.`code` like ? OR tek.`label` like ? OR tek.`type` like ? OR tek.`description` like ?)
+				
+				LIMIT 5' ;
+		
+		$team_list_tmp = $this->db->fetchAll($sql,array('%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%')) ;
 		
 		$sql = 'SELECT t.*
 				FROM techno t
@@ -34,14 +43,22 @@ class SearchService extends BaseService
 				LIMIT 5		' ;
 		$techno_list = $this->db->fetchAll($sql,array('%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%')) ;
 		
-		$sql = 'SELECT u.* FROM users u
-				WHERE
+		$sql = 'SELECT u.* 
+FROM users u 
+LEFT JOIN team_techno tt ON tt.`id_team` = u.`id_team`
+LEFT JOIN techno tek ON tek.`id` = tt.`id_techno` 
+
+WHERE
 				u.`description` like ?
 				OR u.`firstname` like ?
 				OR u.`username` like ?
-				OR u.`twittername` like ? LIMIT 5;';
+				OR u.`twittername` like ?
+OR				
+( tek.`code` like ? OR tek.`label` like ? OR tek.`type` like ? OR tek.`description` like ?)
+
+LIMIT 5';
 		
-		$person_list = $this->db->fetchAll($sql,array('%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%')) ;
+		$person_list = $this->db->fetchAll($sql,array('%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%','%'.$term.'%')) ;
 		
 		return array(
 			'teams' 	=> $team_list,
